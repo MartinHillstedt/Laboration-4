c4b4e47d18ec69f7953324c67717a03d343df481
martinhillstedt <martinhillstedt@gmail.com>
2026-09-25 15:35:47 +0200

    Uppdaterade åldersvariabel och justerade utskrifter i uppgift 3 för korrekt klassificering av ålder med hjälp av if, else if och else.

--- Uppgift3.js
+++ Uppgift3.js
@@ -1,8 +1,15 @@
 //Lösning till uppgift 3. Av Martin Nilsson, 2026
 "use strict";
 
-const age= 42;
-const barn = "Barn";
-const vuxen = "Vuxen";
-const pensionär = "Pensionär";
+const age= 18;
+const barn = "barn.";
+const vuxen = "vuxen.";
+const pensionär = "pensionär.";
 
+if (age < 18) { 
+    console.log(`Mitt eminenta javascript har kommit fram till att du är ett ${barn}`); //Om åldern är mindre än 18 år, skriv ut att personen är ett barn.
+} else if (age >= 18 && age < 65) { 
+    console.log(`Mitt eminenta javascript har kommit fram till att du är en ${vuxen}`); //Om åldern är mellan 18 och 65 år, skriv ut att personen är vuxen.
+} else { 
+    console.log(`Mitt eminenta javascript har kommit fram till att du är en ${pensionär}`); //Om åldern är 65 år eller äldre, skriv ut att personen är pensionär.
+}
