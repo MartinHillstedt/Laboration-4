b802bd442000a3cd1ea4c1eb08c3618191713c49
martinhillstedt <martinhillstedt@gmail.com>
2026-09-25 13:52:39 +0200

    La till kommentar om boolean.

--- Uppgift1.js
+++ Uppgift1.js
@@ -1,10 +1,11 @@
 //Lösning på uppgift 1
+
 "use strict;"
 
 const firstName = "Martin";
 const lastName = "Nilsson";
 const age = 30;
-let isStudent = true;
+let isStudent = true; //En fråga om personen är student i form av boolean.
 
 console.log(`${firstName} ${lastName}`);
 console.log(`Ålder:${age}`);
