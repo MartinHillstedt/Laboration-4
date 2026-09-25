249581feb6628b5343463bedbd7d6d718314ca6b
martinhillstedt <martinhillstedt@gmail.com>
2026-09-25 18:03:03 +0200

    Lade till en for loop för att skriva ut alla udda tal mellan 1-20 med hjälp av modulus i uppgift 4.

--- Uppgift4.js
+++ Uppgift4.js
@@ -5,3 +5,9 @@
 for (let i = 1; i <= 20; i++) {
     console.log(i);
 }
+//Skriver ut alla udda tal mellan 1-20 i konsolen.
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
