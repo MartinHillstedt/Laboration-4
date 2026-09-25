8c0e92257b881c3fee299565ddb55863a91aaae8
martinhillstedt <martinhillstedt@gmail.com>
2026-09-25 16:13:33 +0200

    Uppdaterade åldersvariablerna i uppgift 3 för att slippa å,ä,ö. Lagt till en for loop för uppgift 4 som beräknar och skriver ut talen 1-20.

--- Uppgift3.js
+++ Uppgift3.js
@@ -1,15 +1,15 @@
 //Lösning till uppgift 3. Av Martin Nilsson, 2026
 "use strict";
 
-const age= 18;
-const barn = "barn.";
-const vuxen = "vuxen.";
-const pensionär = "pensionär.";
+const age= 17;
+const child= "barn.";
+const adult= "vuxen.";
+const senior= "pensionär.";
 
 if (age < 18) { 
-    console.log(`Mitt eminenta javascript har kommit fram till att du är ett ${barn}`); //Om åldern är mindre än 18 år, skriv ut att personen är ett barn.
+    console.log(`Mitt eminenta javascript har kommit fram till att du är ett ${child}`); //Om åldern är mindre än 18 år, skriv ut att personen är ett barn.
 } else if (age >= 18 && age < 65) { 
-    console.log(`Mitt eminenta javascript har kommit fram till att du är en ${vuxen}`); //Om åldern är mellan 18 och 65 år, skriv ut att personen är vuxen.
+    console.log(`Mitt eminenta javascript har kommit fram till att du är en ${adult}`); //Om åldern är mellan 18 och 65 år, skriv ut att personen är vuxen.
 } else { 
-    console.log(`Mitt eminenta javascript har kommit fram till att du är en ${pensionär}`); //Om åldern är 65 år eller äldre, skriv ut att personen är pensionär.
+    console.log(`Mitt eminenta javascript har kommit fram till att du är en ${senior}`); //Om åldern är 65 år eller äldre, skriv ut att personen är pensionär.
 }
---
+++ Uppgift4.js
@@ -0,0 +1,7 @@
+//Lösning till uppgift 4. Av Martin Nilsson, 2026
+"use strict";
+
+//Skriver ut talen 1-20 i konsolen.
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
